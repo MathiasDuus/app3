f500c69e0f3c3931054141bbda4a7cd0098dffcd
Mathias Duus <mathias031001@gmail.com>
2023-03-28 11:41:11 +0200

    Add flash on shake

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,59 @@ import 'package:my_app/gps.dart';
 import 'package:my_app/qrReader.dart';
 import 'package:my_app/apiCall.dart';
 import 'package:my_app/takePic.dart';
+import 'package:shake/shake.dart';
+import 'package:torch_light/torch_light.dart';
 import 'package:vibration/vibration.dart';
 
-void main() => runApp(const MaterialApp(home: MyHome()));
+void main() => runApp(const MyApp());
 
-class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  MyHomeState createState() => MyHomeState();
+}
+
+class MyHomeState extends State<MyHome> {
+  bool flashIsOn = false; //to set if flash light is on or off
+  @override
+  void initState() {
+    super.initState();
+    ShakeDetector detector = ShakeDetector.autoStart(
+      onPhoneShake: () {
+        if (flashIsOn) {
+          //if light is on, then turn off
+          TorchLight.disableTorch();
+          setState(() {
+            flashIsOn = false;
+          });
+        } else {
+          //if light is off, then turn on.
+          TorchLight.enableTorch();
+          setState(() {
+            flashIsOn = true;
+          });
+        }
+        // Do stuff on phone shake
+      },
+      minimumShakeCount: 1,
+      shakeSlopTimeMS: 500,
+      shakeCountResetTime: 3000,
+      shakeThresholdGravity: 2.7,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
